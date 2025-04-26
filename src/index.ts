3728b7effccbe066d39f05741b92afa021f0067a
burger-v <nuttanai.bmk@gmail.com>
2025-04-26 11:51:39 +0700

    add insert data

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
 import { AppDataSource } from "./data-source"
+import { Profile } from "./entity/Profile"
+import { Gender } from "./enum/gender.enum"
 
 AppDataSource.initialize().then(async () => {
-
+    await createProfile()
 }).catch(error => console.log(error))
 
-async function createUser() {
-    // noop
+async function createProfile() {
+    const profile = new Profile()
+    profile.displayName = 'John Farmer'
+    profile.gender = Gender.MALE
+    profile.address = {
+        subDistrict: 'Sukhumvit',
+        district: 'Bangkok',
+        province: 'Bangkok',
+    }
+    profile.cost = 1000.8237
+    
+    const savedProfile = await AppDataSource.manager.save(profile)
+    console.log('Profile saved', savedProfile.id)
+
+    console.log('Profile', await getProfile(savedProfile.id))
 }
 
-async function createPost() {
-    // noop
+async function getProfile(id: number) {
+    const profile = await AppDataSource.manager.findBy(Profile, {id})
+    return profile
 }
 
-async function update() {
-    // noop
-}
-
-async function getUserById(id: number) {
-    // noop
-}
-
-async function deleteUser(id: number) {
-    // noop
-}
-
-async function deletePost(id: number) {
-    // noop
-}
